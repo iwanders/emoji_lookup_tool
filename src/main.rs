53f354a3eeb7e3d4a953a4b628bc6ece56626c8e
Ivor Wanders <ivor@iwanders.net>
2026-03-08 13:17:10 -0400

    Creating paths to the noto emoji font.

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,81 @@ impl Entry {
     }
 }
 
+#[derive(Debug, Clone)]
+struct UnicodePoints {
+    points: Vec<u32>,
+}
+impl UnicodePoints {
+    fn to_hex_u16(&self) -> String {
+        // 0030_20e3
+        self.points
+            .iter()
+            .map(|a| format!("{a:0>4x}"))
+            .collect::<Vec<_>>()
+            .join("_")
+    }
+}
+
+#[derive(Debug, Clone)]
+enum PngResolution {
+    Px32,
+    Px72,
+    Px128,
+    Px512,
+}
+#[derive(Debug, Clone)]
+enum NotoGlyphType {
+    Svg,
+    Png(PngResolution),
+}
+impl NotoGlyphType {
+    pub fn to_extension(&self) -> &'static str {
+        match self {
+            NotoGlyphType::Svg => "svg",
+            NotoGlyphType::Png(_) => "png",
+        }
+    }
+    pub fn to_subpath(&self) -> &'static str {
+        match self {
+            NotoGlyphType::Svg => "svg",
+            NotoGlyphType::Png(v) => match v {
+                PngResolution::Px32 => "png/32",
+                PngResolution::Px72 => "png/72",
+                PngResolution::Px128 => "png/128",
+                PngResolution::Px512 => "png/512",
+            },
+        }
+    }
+}
+
+struct NotoFont {}
+impl NotoFont {
+    const BASE_URL: &'static str = "https://raw.githubusercontent.com/googlefonts/noto-emoji";
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub fn to_path(&self, v: &str) -> UnicodePoints {
+        let mut points = vec![];
+        for c in v.chars() {
+            points.push(c as u32);
+        }
+        UnicodePoints { points }
+    }
+    pub fn to_url(&self, codepoints: &UnicodePoints, format: NotoGlyphType) -> String {
+        // https://raw.githubusercontent.com/googlefonts/noto-emoji/refs/heads/main/svg/emoji_u0023.svg
+        // https://raw.githubusercontent.com/googlefonts/noto-emoji/refs/tags/v2.051/svg/emoji_u0023.svg
+        // https://raw.githubusercontent.com/googlefonts/noto-emoji/8998f5dd683424a73e2314a8c1f1e359c19e8742/svg/emoji_u0023.svg
+        // Lets just use main.
+        let ext = format.to_extension();
+        let subpath = format.to_subpath();
+        format!(
+            "{}/refs/heads/main/{subpath}/emoji_u{}.{ext}",
+            Self::BASE_URL,
+            codepoints.to_hex_u16()
+        )
+    }
+}
+
 #[derive(Parser)]
 #[command(version, about, long_about = "Search emoji's.")]
 #[command(propagate_version = true)]
@@ -60,6 +135,10 @@ struct Cli {
 enum Commands {
     /// Search emoji's, matching by the provided string in name and short names.
     Search { name: String },
+
+    /// Retrieve the emoji from the noto font github repo at https://github.com/googlefonts/noto-emoji
+    Noto { emoji: String },
+
     /// Dump the entire emoji list, because why not, then you can grep to your heart's content.
     List,
 }
@@ -92,6 +171,15 @@ fn main() {
                     e.subcategory
                 );
             }
+        }
+        Commands::Noto { emoji } => {
+            let noto = NotoFont::new();
+            let path = noto.to_path(emoji);
+            println!("Converted {emoji} to: {path:?}");
+            println!(
+                "url: {:?}",
+                noto.to_url(&path, NotoGlyphType::Png(PngResolution::Px128))
+            );
         }
     }
 }
